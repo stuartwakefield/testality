095118cd3ebaac68d0cd86bb890480386649089a
Stuart Wakefield <stuie@live.co.uk>
2013-03-21 09:36:16 +0000

    Removing unnecessary class

--- lib/testality.rb
+++ lib/testality.rb
@@ -3,32 +3,37 @@ require "testality/watcher"
 require "testality/resources"
 require "testality/websocket"
 
+require "testality/headers"
+
 module Testality
 
 	class Serve
 	
 		def initialize(port, mport, srcs)
 
-			resources = Testality::Resources.new srcs
-			websocket = Testality::WebSocket.new mport
-			server = Testality::Server.new port, resources, websocket
-			watcher = Testality::Watcher.new resources, server
+			headers = Testality::Headers.new "something"
+			print headers.name
 
-			websocketThread = Thread.fork websocket do |ws|
-				ws.start
-			end
+#			resources = Testality::Resources.new srcs
+#			websocket = Testality::WebSocket.new mport
+#			server = Testality::Server.new port, resources, websocket
+#			watcher = Testality::Watcher.new resources, server
 
-			watcherThread = Thread.fork watcher do |w|
-				w.start 
-			end
+#			websocketThread = Thread.fork websocket do |ws|
+#				ws.start
+#			end
 
-			serverThread = Thread.fork server do |s|
-				s.start
-			end
+#			watcherThread = Thread.fork watcher do |w|
+#				w.start 
+#			end
 
-			watcherThread.join
-			serverThread.join
-			websocketThread.join
+#			serverThread = Thread.fork server do |s|
+#				s.start
+#			end
+
+#			watcherThread.join
+#			serverThread.join
+#			websocketThread.join
 			
 		end
 	
