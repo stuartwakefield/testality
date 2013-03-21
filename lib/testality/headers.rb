af87728ef42b4eaa82899e5fdc537ac2d1d66ba2
Stuart Wakefield <stuie@live.co.uk>
2013-03-21 09:05:30 +0000

    Updates

--- lib/testality/headers.rb
+++ lib/testality/headers.rb
@@ -1,10 +1,15 @@
 module Testality
 	class Headers
 		def initialize(name)
-			set_name name
+			set_name do 
+				name
+			end
 		end
-		def set_name(name)
-			@name = name
+		def set_name
+			@name = yield
+		end
+		def name
+			@name
 		end
 	end
 end
